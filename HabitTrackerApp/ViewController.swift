7a5ffc111011d150abf1b318780b4cd2352dfc36
Rus <ruzaki.dev@gmail.com>
2026-01-05 17:06:12 -0500

    Initial Commit

---
+++ HabitTrackerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HabitTrackerApp
+//
+//  Created by Rus Zakirov on 05.01.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
