cabfea4ce73739e2ff03158c6e28bc70c03e4fe6
Rus <ruzaki.dev@gmail.com>
2026-01-05 23:44:51 -0500

    Step 0: create Habits and Info view controllers

---
+++ HabitTrackerApp/HabitsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  HabitsViewController.swift
+//  HabitTrackerApp
+//
+//  Created by Rus Zakirov on 05.01.2026.
+//
+
+import UIKit
+
+class HabitsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+        
+        title = "Сегодня"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.largeTitleDisplayMode = .always
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ HabitTrackerApp/InfoViewController.swift
@@ -0,0 +1,33 @@
+//
+//  InfoViewController.swift
+//  HabitTrackerApp
+//
+//  Created by Rus Zakirov on 05.01.2026.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+        
+        title = "Информация"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.largeTitleDisplayMode = .always
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
