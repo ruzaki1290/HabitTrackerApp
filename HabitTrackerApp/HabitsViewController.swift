02bb8e917fb0c50450f2e589f391b61cc8d4ba82
Rus <ruzaki.dev@gmail.com>
2026-01-09 19:42:37 -0500

    Set up tab bar navigation and Info screen with scrollable content

--- HabitTrackerApp/HabitsViewController.swift
+++ HabitTrackerApp/HabitsViewController.swift
@@ -10,24 +10,31 @@ import UIKit
 class HabitsViewController: UIViewController {
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
         
-        title = "Привычки"
+        title = "Сегодня"
         navigationController?.navigationBar.prefersLargeTitles = true
+        // Large titles
         navigationItem.largeTitleDisplayMode = .always
+        // Добавляем кнопку “+”
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+                    barButtonSystemItem: .add,
+                    target: self,
+                    action: #selector(addHabitTapped)
+                )
+        
     }
     
+    @objc private func addHabitTapped() {
+            let alert = UIAlertController(title: "Добавить привычку",
+                                          message: "Скоро тут будет экран создания привычки 🙂",
+                                          preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ок", style: .default))
+            present(alert, animated: true)
+        }
+    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
+} // class HabitsViewController
