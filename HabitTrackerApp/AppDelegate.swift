be7a0fb3e635a85dcf3d2abcc95f1963a86c32d2
Rus <ruzaki.dev@gmail.com>
2026-01-22 23:42:03 -0500

    Add LaunchScreen with centered icon and title, configure assets and fix layout constraints

--- HabitTrackerApp/AppDelegate.swift
+++ HabitTrackerApp/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Скорость показа заставки(LaunchScreen)
-        Thread.sleep(forTimeInterval: 0.8)
+        Thread.sleep(forTimeInterval: 1.6)
         // Override point for customization after application launch.
         return true
     }
